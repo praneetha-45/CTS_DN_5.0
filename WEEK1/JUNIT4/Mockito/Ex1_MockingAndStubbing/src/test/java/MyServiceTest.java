c93bb232e6a35d203044c87bc503c878bc746853
Praneetha Rampudi <23501a4450@pvpsit.ac.in>
2026-06-28 17:56:02 +0530

    Added Mockito Exercise 1 Mocking and Stubbing

---
+++ WEEK1/JUNIT4/Mockito/Ex1_MockingAndStubbing/src/test/java/MyServiceTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+
+        // Create Mock
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        // Stub Method
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        // Inject Mock
+        MyService service = new MyService(mockApi);
+
+        // Test
+        String result = service.fetchData();
+
+        // Assertion
+        assertEquals("Mock Data", result);
+    }
+
+}

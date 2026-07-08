2bcf34e5e2dd13677d5edd2d31f81a1878379414
Praneetha Rampudi <23501a4450@pvpsit.ac.in>
2026-07-08 19:23:27 +0530

    Added Week3 Spring REST Ex-05 Get Country By Code

---
+++ WEEK3/SPRING_REST/Ex-05 Get Country Based on Country Code/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,76 @@
+package com.cognizant.springlearn.controller;
+
+
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.cognizant.springlearn.model.Country;
+
+import com.cognizant.springlearn.service.CountryService;
+
+
+
+@RestController
+
+public class CountryController {
+
+
+
+private static final Logger LOGGER=
+
+LoggerFactory.getLogger(CountryController.class);
+
+
+
+@Autowired
+
+CountryService service;
+
+
+
+@GetMapping("/countries/{code}")
+
+public Country getCountry(
+
+@PathVariable String code){
+
+
+
+LOGGER.info("START");
+
+
+
+Country country=
+
+service.getCountry(
+
+code.toUpperCase()
+
+);
+
+
+
+LOGGER.info("END");
+
+
+
+return country;
+
+
+}
+
+
+
+}
